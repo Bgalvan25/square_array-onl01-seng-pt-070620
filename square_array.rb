69ed0b833fea7bf84a0095f2d61cf3e275ef51dd
Brittney Galvan <brittney.galvan25@yahoo.com>
2020-07-12 17:06:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   new_num = [ "1" , "2" , "3"] 						# Declared new array 
    array.each do |num|
     new_num << num ** 2      # Each element squared
-    return square_array(new_num)				  # Returns the array AND stops the iteration
+    return new_num				  # Returns the array AND stops the iteration
   end
 end
